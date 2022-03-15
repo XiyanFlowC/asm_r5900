ed1606636c125905999a865fbcc2c8e183918954
XiyanFlowC <1197129380@qq.com>
2022-03-16 01:31:37 +0800

    fixed bugs, improve functions. fixed a bug in reg profile about SN. added a little type parse related codes about anal.

--- anal/p/anal_r5900.c
+++ anal/p/anal_r5900.c
@@ -14,10 +14,230 @@ static int r5900_op(RAnal *anal, RAnalOp *op, ut64 addr, const ut8 *data, int le
     op->size = 4;
 
     struct instr_t tmp = DecodeInstruction(instr);
+    long long imm = *(long long*)&tmp.imm;
+    int opcode = *(int*)&tmp.opcode;
 
     switch(tmp.opcode) {
         case NOP:
         op->type = R_ANAL_OP_TYPE_NOP;
+        break;
+        case SYSCALL:
+        op->type = R_ANAL_OP_TYPE_SWI;
+        break;
+        case BREAK:
+        op->type = R_ANAL_OP_TYPE_TRAP;
+        break;
+
+        /* jump */
+        case J:
+        op->type = R_ANAL_OP_TYPE_JMP;
+        op->jump = tmp.imm;
+        op->delay = 1;
+        r_strbuf_setf (&op->esil, "0x%llx,pc,=", imm);
+        break;
+        case JAL:
+        op->type = R_ANAL_OP_TYPE_CALL;
+        op->jump = tmp.imm;
+        op->delay = 1;
+        op->fail = addr + 8;
+        r_strbuf_setf (&op->esil, "0x%llx,pc,=,ra,=", imm);
+        break;
+        case JALR:
+        op->type = R_ANAL_OP_TYPE_UCALL;
+        op->jump = tmp.imm;
+        op->delay = 1;
+        op->fail = addr + 8;
+        r_strbuf_setf (&op->esil, "0x%llx,pc,=,%s,=", imm, gpr_names[tmp.rd]);
+        break;
+        case JR:
+        if (tmp.rs == 31) { // jr ra
+            op->type = R_ANAL_OP_TYPE_RET;
+        }
+        else { // reg jmp - switch etc.
+            op->type = R_ANAL_OP_TYPE_RJMP;
+        }
+        
+        op->delay = 1;
+        op->dst = r_anal_value_new();
+        op->src[0] = r_anal_value_new();
+        op->dst->reg = r_reg_get (anal->reg, "pc",  R_REG_TYPE_GPR);
+        op->src[0]->reg = r_reg_get (anal->reg, gpr_names[tmp.rs], R_REG_TYPE_GPR);
+        r_strbuf_set (&op->esil, gpr_names[tmp.rs]);
+        r_strbuf_append (&op->esil, ",pc,:=");
+        // op->reg = gpr_names[tmp.rs];
+        break;
+
+        /* branch */
+        case BEQ:
+        case BEQL:
+        case BGEZ:
+        case BGEZL:
+        case BGEZAL:
+        case BGEZALL:
+        case BGTZ:
+        case BGTZL:
+        case BLEZ:
+        case BLEZL:
+        case BLTZ:
+        case BLTZL:
+        case BLTZAL:
+        case BLTZALL:
+        case BNE:
+        case BNEL:
+        op->type = R_ANAL_OP_TYPE_CJMP;
+        op->delay = 1;
+        op->jump = addr + 4 + imm;
+        op->fail = addr + 8;
+        switch(opcode) { // TODO: Add emulation codes
+            case BEQL:
+            op->delay = 0;
+            case BEQ:
+            op->cond = R_ANAL_COND_EQ;
+            break;
+            case BGEZALL:
+            op->delay = 0;
+            case BGEZAL:
+            op->type = R_ANAL_OP_TYPE_CCALL;
+            op->cond = R_ANAL_COND_GE;
+            break;
+            case BGEZL:
+            op->delay = 0;
+            case BGEZ:
+            op->cond = R_ANAL_COND_GE;
+            break;
+            case BGTZL:
+            op->delay = 0;
+            case BGTZ:
+            op->cond = R_ANAL_COND_GT;
+            break;
+            case BLTZL:
+            op->delay = 0;
+            case BLTZ:
+            op->cond = R_ANAL_COND_LT;
+            break;
+            case BLTZALL:
+            op->delay = 0;
+            case BLTZAL:
+            op->cond = R_ANAL_COND_LT;
+            op->type = R_ANAL_OP_TYPE_CCALL;
+            break;
+            case BNEL:
+            op->delay = 0;
+            case BNE:
+            op->cond = R_ANAL_COND_NE;
+            break;
+        }
+        break;
+
+        /* load and store */
+        case LB:
+        case LBU:
+        op->refptr = 1;
+        case LH:
+        case LHU:
+        if(!op->refptr)
+            op->refptr = 2;
+        case LW:
+        case LWU:
+        if(!op->refptr)
+            op->refptr = 4;
+        case LD: // TODO: LDL, LDR, LWL, LWR
+        if(!op->refptr)
+            op->refptr = 8;
+        case LQ:
+        if(!op->refptr)
+            op->refptr = 16;
+
+        if(tmp.rs == 28) {
+            op->ptr = anal->gp + imm;
+        }
+        else {
+            op->ptr = imm + r_reg_getv(anal->reg, gpr_names[tmp.rs]);
+        }
+        op->dst = r_anal_value_new();
+        op->dst->reg = r_reg_get (anal->reg, gpr_names[tmp.rt], R_REG_TYPE_GPR);
+        op->src[0] = r_anal_value_new();
+        op->src[0]->reg = r_reg_get (anal->reg, gpr_names[tmp.rs], R_REG_TYPE_GPR);
+        op->src[0]->delta = imm;
+        // r_strbuf_set (&op->esil, "")
+        op->type = R_ANAL_OP_TYPE_LOAD;
+        op->reg = gpr_names[tmp.rt];
+        op->ireg = gpr_names[tmp.rs];
+        op->val = *(uint64_t*)&imm;
+        op->sign = true;
+        break;
+        case SB: // TODO: add esil and emu code
+        op->refptr = 1;
+        case SH:
+        if(!op->refptr)
+            op->refptr = 2;
+        case SW:
+        if(!op->refptr)
+            op->refptr = 4;
+        case SD:
+        if(!op->refptr)
+            op->refptr = 8;
+        case SQ:
+        if(!op->refptr)
+            op->refptr = 16;
+
+        if(tmp.rs == 28) { // gp
+            op->ptr = anal->gp + imm;
+        }
+        else {
+            op->ptr = imm + r_reg_getv(anal->reg, gpr_names[tmp.rs]);
+        }
+        op->dst = r_anal_value_new();
+        op->src[0] = r_anal_value_new();
+        op->dst->delta = imm;
+        op->dst->reg = r_reg_get (anal->reg, gpr_names[tmp.rs], R_REG_TYPE_GPR);
+        op->src[0]->reg = r_reg_get (anal->reg, gpr_names[tmp.rt], R_REG_TYPE_GPR);
+        // r_strbuf_set (&op->esil, "")
+        op->type = R_ANAL_OP_TYPE_STORE;
+        break;
+
+        /* arithmetic */
+        case LUI:
+        op->dst = r_anal_value_new();
+        op->dst->reg = r_reg_get (anal->reg, gpr_names[tmp.rt], R_REG_TYPE_GPR);
+        op->val = imm;
+        r_strbuf_setf (&op->esil, "4,0x%llX,0xffff,&,<<,%s,=", imm, gpr_names[tmp.rt]);
+        break;
+        case ADD:
+        case ADDU:
+        case DADD:
+        case DADDU:
+        op->dst = r_anal_value_new();
+        op->src[0] = r_anal_value_new();
+        op->src[1] = r_anal_value_new();
+        op->dst->reg = r_reg_get(anal->reg, gpr_names[tmp.rd], R_REG_TYPE_GPR);
+        op->src[0]->reg = r_reg_get(anal->reg, gpr_names[tmp.rs], R_REG_TYPE_GPR);
+        op->src[1]->reg = r_reg_get(anal->reg, gpr_names[tmp.rt], R_REG_TYPE_GPR);
+        op->type = R_ANAL_OP_TYPE_ADD;
+
+        if (tmp.opcode == DADDU && tmp.rt == 0) { // move ( daddu rd, rs, zero)
+            op->type = R_ANAL_OP_TYPE_MOV;
+            r_anal_value_free (op->src[1]);
+            op->src[1] = NULL;
+        }
+        r_strbuf_setf (&op->esil, "%s,%s,%s,+,=",
+            gpr_names[tmp.rs],
+            gpr_names[tmp.rt],
+            gpr_names[tmp.rd]);// TODO: these instr should not share same logic
+        break;
+        case ADDIU:
+        case ADDI:
+        op->dst = r_anal_value_new();
+        op->src[0] = r_anal_value_new();
+        op->src[1] = r_anal_value_new();
+        op->dst->reg = r_reg_get(anal->reg, gpr_names[tmp.rd], R_REG_TYPE_GPR);
+        op->src[0]->reg = r_reg_get(anal->reg, gpr_names[tmp.rs], R_REG_TYPE_GPR);
+        op->val = imm;
+        op->type = R_ANAL_OP_TYPE_ADD;
+        r_strbuf_setf (&op->esil, "%s,%lld,%s,+,=",
+            gpr_names[tmp.rs],
+            imm,
+            gpr_names[tmp.rd]);// TODO: these instr should not share same logic
         break;
     }
 
@@ -33,6 +253,7 @@ static bool set_r5900_reg_profile(RAnal* anal) {
     "=A1    a1\n"
     "=A2    a2\n"
     "=A3    a3\n"
+    "=SN    v0\n"
     "gpr    zero    .64 0   0\n"
     "gpr    zeroh   .64 8   0\n"
     "gpr    at  .64 16  0\n"
@@ -75,9 +296,9 @@ static bool set_r5900_reg_profile(RAnal* anal) {
     "gpr    sph .64 312 0\n"
 	"gpr	fp	.64	320	0\n"
     "gpr    fph .64 328 0\n"
-	"gpr	ra	.64	336	0\n"
-    "gpr    rah .64 344 0\n"
-	"gpr	pc	.64	352	0\n";
+	"gpr	ra	.64	336	0\n" // 32 bits ?
+//    "gpr    rah .64 344 0\n"
+	"gpr	pc	.64	344	0\n"; // 32 bits ?
     return r_reg_set_profile_string(anal->reg, p);
 }
 
@@ -94,7 +315,7 @@ RAnalPlugin r_anal_plugin_r5900 = {
     .bits = 32,
     .op = r5900_op,
     .archinfo = r5900_archinfo,
-    .esil = false,
+    .esil = true,
     .set_reg_profile = set_r5900_reg_profile,
 };
 
--- asm/p/asm_r5900.c
+++ asm/p/asm_r5900.c
@@ -49,28 +49,6 @@ int _get_target_(const char *str) {
     }
 }
 
-const char *gpr_names[] = {
-    "zero", "at", "v0", "v1", "a0", "a1", "a2", "a3",
-    "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7",
-    "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7",
-    "t8", "t9", "k0", "k1", "gp", "sp", "fp", "ra",
-};
-
-const char *cop0r_names[] = {
-    "Index", "Random", "EntryLo0", "EntryLo1", "Context", "PageMask",
-    "Wired", "Reserved0", "BadVAddr", "Count", "EntryHi", "Compare",
-    "Status", "Cause", "EPC", "PRId", "Config", "COP0R17", "COP0R18", "COP0R19",
-    "COP0R20", "COP0R21", "COP0R22", "COP0R23", "Debug", "Perf",
-    "COP0R26", "COP0R27", "TagLo", "TagHi", "ErrorEPC", "COP0R31",
-};
-
-const char *cop1r_names[] = {
-    "f00", "f01", "f02", "f03", "f04", "f05", "f06", "f07",
-    "f08", "f09", "f10", "f11", "f12", "f13", "f14", "f15",
-    "f16", "f17", "f18", "f19", "f20", "f21", "f22", "f23",
-    "f24", "f25", "f26", "F27", "f28", "f29", "f30", "f31",
-};
-
 int disas(struct instr_t instr, ut32 offset, char *buffer) {
     char mnemonic[128];
     char para_buf[512];
